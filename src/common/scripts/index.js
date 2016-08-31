491c88245c5d0cf7fb8bf246ef6263e1ae00d8fb
Ray Patterson <ray_patterson@apple.com>
2016-08-31 13:40:00 -0700

    feat(modernizr): Added custom Modernizr build.

--- src/common/scripts/index.js
+++ src/common/scripts/index.js
@@ -2,4 +2,12 @@
 
 require('fastclick').attach(document.body);
 
+var Modernizr = require('modernizr');
+
+if (Modernizr.promises) {
+
+	console.log('Promises can be made!');
+
+}
+
 console.log('common');
